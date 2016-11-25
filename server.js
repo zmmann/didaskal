e36b700ab91daec7ef8d8b010553e38c9a6f8056
Bihn Kim <bihnkim@gmail.com>
2016-11-25 00:28:28 -0800

    modify package.json and follow conventional npm start script

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const app = express();
 
+// default and only GET request serves index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// static assets are found in PUBLIC directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
